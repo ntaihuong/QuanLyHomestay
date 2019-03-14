3b5dea355a1682e3d46176c6e8bb801a7ebcb389
ntaihuong <35464166+ntaihuong@users.noreply.github.com>
2019-03-15 01:38:41 +0700

    Thiết kế giao diện fmdk

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace HomestayApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fmLogin());
+            Application.Run(new fmDangKi());
         }
     }
 }
---
+++ fmDangKi.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace HomestayApp
+{
+    partial class fmDangKi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.txtXNMK = new System.Windows.Forms.TextBox();
+            this.lbXNMK = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.lbMhatkhau = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtTaikhoan = new System.Windows.Forms.TextBox();
+            this.lbTaikhoan = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangKi = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtHoten = new System.Windows.Forms.TextBox();
+            this.lbHoten = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.btnThoat);
+            this.panel1.Controls.Add(this.btnDangKi);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(5, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 340);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.txtXNMK);
+            this.panel6.Controls.Add(this.lbXNMK);
+            this.panel6.Location = new System.Drawing.Point(3, 233);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(335, 40);
+            this.panel6.TabIndex = 7;
+            // 
+            // txtXNMK
+            // 
+            this.txtXNMK.Location = new System.Drawing.Point(133, 12);
+            this.txtXNMK.Name = "txtXNMK";
+            this.txtXNMK.Size = new System.Drawing.Size(188, 20);
+            this.txtXNMK.TabIndex = 1;
+            // 
+            // lbXNMK
+            // 
+            this.lbXNMK.AutoSize = true;
+            this.lbXNMK.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbXNMK.Location = new System.Drawing.Point(3, 11);
+            this.lbXNMK.Name = "lbXNMK";
+            this.lbXNMK.Size = new System.Drawing.Size(114, 16);
+            this.lbXNMK.TabIndex = 0;
+            this.lbXNMK.Text = "Xác nhật mật khẩu";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.txtMatKhau);
+            this.panel7.Controls.Add(this.lbMhatkhau);
+            this.panel7.Location = new System.Drawing.Point(3, 187);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(335, 40);
+            this.panel7.TabIndex = 6;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(133, 12);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(188, 20);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // lbMhatkhau
+            // 
+            this.lbMhatkhau.AutoSize = true;
+            this.lbMhatkhau.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMhatkhau.Location = new System.Drawing.Point(3, 11);
+            this.lbMhatkhau.Name = "lbMhatkhau";
+            this.lbMhatkhau.Size = new System.Drawing.Size(62, 16);
+            this.lbMhatkhau.TabIndex = 0;
+            this.lbMhatkhau.Text = "Mật khẩu";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.txtTaikhoan);
+            this.panel4.Controls.Add(this.lbTaikhoan);
+            this.panel4.Location = new System.Drawing.Point(3, 141);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(335, 40);
+            this.panel4.TabIndex = 5;
+            // 
+            // txtTaikhoan
+            // 
+            this.txtTaikhoan.Location = new System.Drawing.Point(133, 12);
+            this.txtTaikhoan.Name = "txtTaikhoan";
+            this.txtTaikhoan.Size = new System.Drawing.Size(188, 20);
+            this.txtTaikhoan.TabIndex = 1;
+            // 
+            // lbTaikhoan
+            // 
+            this.lbTaikhoan.AutoSize = true;
+            this.lbTaikhoan.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTaikhoan.Location = new System.Drawing.Point(3, 11);
+            this.lbTaikhoan.Name = "lbTaikhoan";
+            this.lbTaikhoan.Size = new System.Drawing.Size(62, 16);
+            this.lbTaikhoan.TabIndex = 0;
+            this.lbTaikhoan.Text = "Tài khoản";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.txtPhone);
+            this.panel5.Controls.Add(this.lbPhone);
+            this.panel5.Location = new System.Drawing.Point(3, 95);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(335, 40);
+            this.panel5.TabIndex = 4;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(133, 12);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(188, 20);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.AutoSize = true;
+            this.lbPhone.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhone.Location = new System.Drawing.Point(3, 11);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(42, 16);
+            this.lbPhone.TabIndex = 0;
+            this.lbPhone.Text = "Phone";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(188, 293);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 30);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnDangKi
+            // 
+            this.btnDangKi.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKi.Location = new System.Drawing.Point(79, 293);
+            this.btnDangKi.Name = "btnDangKi";
+            this.btnDangKi.Size = new System.Drawing.Size(75, 30);
+            this.btnDangKi.TabIndex = 2;
+            this.btnDangKi.Text = "Đăng Ký";
+            this.btnDangKi.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtEmail);
+            this.panel3.Controls.Add(this.lbEmail);
+            this.panel3.Location = new System.Drawing.Point(3, 49);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(335, 40);
+            this.panel3.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(133, 12);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(188, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmail.Location = new System.Drawing.Point(3, 11);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(36, 16);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtHoten);
+            this.panel2.Controls.Add(this.lbHoten);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(335, 40);
+            this.panel2.TabIndex = 0;
+            // 
+            // txtHoten
+            // 
+            this.txtHoten.Location = new System.Drawing.Point(133, 12);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.Size = new System.Drawing.Size(188, 20);
+            this.txtHoten.TabIndex = 1;
+            // 
+            // lbHoten
+            // 
+            this.lbHoten.AutoSize = true;
+            this.lbHoten.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHoten.Location = new System.Drawing.Point(3, 11);
+            this.lbHoten.Name = "lbHoten";
+            this.lbHoten.Size = new System.Drawing.Size(45, 16);
+            this.lbHoten.TabIndex = 0;
+            this.lbHoten.Text = "Họ tên";
+            // 
+            // fmDangKi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 364);
+            this.Controls.Add(this.panel1);
+            this.Name = "fmDangKi";
+            this.Text = "fmDangKi";
+            this.panel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox txtXNMK;
+        private System.Windows.Forms.Label lbXNMK;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label lbMhatkhau;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtTaikhoan;
+        private System.Windows.Forms.Label lbTaikhoan;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangKi;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtHoten;
+        private System.Windows.Forms.Label lbHoten;
+    }
+}
