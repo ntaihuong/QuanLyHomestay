f246e133c3b92e349d40c40045644f907ebf5f2d
NguyenThiDiemHuong <nguyenthidiemhuong.3004@gmail.com>
2019-03-16 16:48:11 +0700

    Thiết kế fmLogin_1

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -114,6 +114,16 @@ namespace HomestayApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap images {
+            get {
+                object obj = ResourceManager.GetObject("images", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap NC1 {
             get {
                 object obj = ResourceManager.GetObject("NC1", resourceCulture);
--- fmLogin.Designer.cs
+++ fmLogin.Designer.cs
@@ -37,49 +37,56 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.lbTDN = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
             this.panel1.Controls.Add(this.btnThoat);
             this.panel1.Controls.Add(this.btnDangNhap);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(12, 26);
+            this.panel1.Location = new System.Drawing.Point(191, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(341, 146);
+            this.panel1.Size = new System.Drawing.Size(347, 197);
             this.panel1.TabIndex = 0;
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(249, 106);
+            this.btnThoat.BackColor = System.Drawing.Color.White;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThoat.Location = new System.Drawing.Point(244, 135);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.Size = new System.Drawing.Size(85, 42);
             this.btnThoat.TabIndex = 3;
             this.btnThoat.Text = "Thoát";
-            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.UseVisualStyleBackColor = false;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // btnDangNhap
             // 
-            this.btnDangNhap.Location = new System.Drawing.Point(150, 106);
+            this.btnDangNhap.BackColor = System.Drawing.Color.White;
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDangNhap.Location = new System.Drawing.Point(139, 135);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.Size = new System.Drawing.Size(85, 42);
             this.btnDangNhap.TabIndex = 2;
             this.btnDangNhap.Text = "Đăng Nhập";
-            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.UseVisualStyleBackColor = false;
             this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.txtPassWord);
             this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(3, 49);
+            this.panel3.Location = new System.Drawing.Point(6, 73);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(335, 40);
+            this.panel3.Size = new System.Drawing.Size(341, 40);
             this.panel3.TabIndex = 1;
             // 
             // txtPassWord
@@ -92,10 +99,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(6, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.Size = new System.Drawing.Size(66, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mật khẩu";
             // 
@@ -103,9 +110,10 @@
             // 
             this.panel2.Controls.Add(this.txtUserName);
             this.panel2.Controls.Add(this.lbTDN);
-            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.panel2.Location = new System.Drawing.Point(6, 27);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(335, 40);
+            this.panel2.Size = new System.Drawing.Size(341, 40);
             this.panel2.TabIndex = 0;
             // 
             // txtUserName
@@ -118,18 +126,30 @@
             // lbTDN
             // 
             this.lbTDN.AutoSize = true;
-            this.lbTDN.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTDN.Location = new System.Drawing.Point(3, 11);
+            this.lbTDN.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbTDN.Location = new System.Drawing.Point(6, 13);
             this.lbTDN.Name = "lbTDN";
-            this.lbTDN.Size = new System.Drawing.Size(124, 19);
+            this.lbTDN.Size = new System.Drawing.Size(103, 16);
             this.lbTDN.TabIndex = 0;
             this.lbTDN.Text = "Tên đăng nhập";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HomestayApp.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 197);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // fmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 186);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(542, 201);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Name = "fmLogin";
             this.Text = "fmLogin";
@@ -138,6 +158,7 @@
             this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,5 +174,6 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Label lbTDN;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
