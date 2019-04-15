ba7c8578f6d48b83c911b37af345f3af31c00108
ntaihuong <35464166+ntaihuong@users.noreply.github.com>
2019-04-15 09:46:57 +0700

    Edit & Update

--- Login.cs
+++ Login.cs
@@ -9,22 +9,17 @@ namespace HomestayApp
 {
     public static class Login
     {
-        private static QuanLyHomestayEntities db = new QuanLyHomestayEntities();
+        private static QuanLyHomestayEntities db;
         public static bool testLogin(string tk, string mk)
         {
+            db = new QuanLyHomestayEntities();
            
-           try
-            {
                 TAIKHOAN s = (from i in db.TAIKHOANs where i.TaiKhoan1 == tk && i.Matkhau == mk select i).FirstOrDefault();
                 if(s!=null)
                 {
                     return true;
                 }
-            }
-            catch
-            {
-                
-            }
+           
             return false;
         }
         
